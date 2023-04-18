6f04528b075227b883dd2fa910b5ddf92a33f0e4
JInvic <77521861+Jinvic@users.noreply.github.com>
2023-04-18 15:47:50 +0800

    Update mainwindow.cpp

--- DES/Qt/mainwindow.cpp
+++ DES/Qt/mainwindow.cpp
@@ -63,14 +63,14 @@ void MainWindow::run()
     for(int i = 0; i + klen < 8; i++)
         memset(key_buf, 0, sizeof(char));
 
-    char* cipher_buf = (char*) calloc(len + 1, sizeof(char));
+    char* cipher_buf = (char*) calloc(2 * len + 1, sizeof(char));
 
     main_func(plaintext_buf, cipher_buf, len, key_buf, cm);
 
     if (cm == encode)
         // 预处理为可显示字符，每次取4位,即每一字节拆成两个字符
         encode_visable_func(cipher_buf, &len);
-    cipher_buf[len + 1] = '\0';
+    cipher_buf[len] = '\0';
 
     QString cipher = QString(QLatin1String(cipher_buf));
     ui->output_text->setText(cipher);
